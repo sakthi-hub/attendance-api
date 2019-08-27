c12b8f21d9d85959bf5f50ce72ce9c5da89da8ae
sakthi-hub <52353312+sakthi-hub@users.noreply.github.com>
2019-08-27 12:11:46 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,11 @@ const router = express.Router();
 
 var ctrlEmp = require('../controller/emp-details');
 
-
 // profile
-// router.get('/emp_details', ctrlEmp.emp_details);
+router.get('/emp_details_get', ctrlEmp.emp_details_get);
 
 // authentication
 router.post('/emp_details_add', ctrlEmp.emp_details_add);
-// router.post('/emp_details_edit', ctrlEmp.emp_details_edit);
-// router.post('/emp_details_edit',ctrlEmp.emp_details_edit);
+//router.post('/emp_details_edit', ctrlEmp.emp_details_edit);
 
 module.exports = router;
